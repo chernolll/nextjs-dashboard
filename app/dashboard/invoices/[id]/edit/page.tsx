c3200718fc0d3eb4f038aece0ba73635c13d3522
chernolll <bamboowxj@gmail.com>
2025-10-23 15:20:52 +0800

    fix(config): 修复cacheComponent开启后,打包报错的问题

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,4 @@
+'use server';
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
